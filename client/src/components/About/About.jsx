08407f5035cf1c2103b2bc9156b072dd362a7fb3
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-11-03 14:52:42 -0800

    added projects and about components, uncomplete

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -3,13 +3,13 @@ import './About.scss';
 
 function About() {
     return (
-        <div>
-            <div>
-                <img src="" alt="Kaleb Armbrust"/>
+        <div className="about">
+            <div className="about-left">
+                <img className="about-left__image" src="" alt="Kaleb Armbrust"/>
             </div>
-            <div>
-                <p>
-
+            <div className="about-right">
+                <p className="about-right__text">
+                
                 </p>
             </div>
         </div>
---
+++ client/src/components/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Projects.scss';
+import Chatto from '../../assets/images/chatto.png';
+
+function Projects() {
+    return (
+        <div className="projects">
+            <div className="projects-top">
+                <h2 className="projects-top__title">Projects</h2>
+            </div>
+            <div className="projects-bottom">
+                <h3 className="projects-bottom__title">Chatto</h3>
+                <img className="projects-bottom__image" src={Chatto} alt="Chatto"/>
+                <p className="projects-bottom__text"></p>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
