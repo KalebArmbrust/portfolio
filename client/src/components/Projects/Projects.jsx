05aa208de98d371dcb2f63bfa5b21a93e3ad2ac2
Kaleb Armbrust <armbrustcopyco@gmail.com>
2020-11-04 09:13:37 -0800

    setup of server

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
                 <h2 className="projects-top__title">Projects</h2>
             </div>
             <div className="projects-bottom">
-                <h3 className="projects-bottom__title">Chatto</h3>
+                <a href="https://limitless-reef-88204.herokuapp.com/"><h3 className="projects-bottom__title">Chatto</h3></a>
                 <img className="projects-bottom__image" src={Chatto} alt="Chatto"/>
                 <p className="projects-bottom__text">A chat application that pairs English and Japanese speakers together to practice each other's language.</p>
             </div>
