64b0452d826d6b800e4c86fa5e450d795d624690
Kaleb Armbrust <armbrustcopyco@gmail.com>
2021-01-13 20:49:19 -0800

    fixed image, added hover on text

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './About.scss';
-import Koi from '../../assets/images/koi.jpg';
 
 function About() {
     return (
         <div className="about">
             <div className="about-left">
-                <img className="about-left__image" src={Koi} alt="Kaleb Armbrust"/>
-            </div>
-            <div className="about-right">
-                <p className="about-right__text">
-                    My name is Kaleb Armbrust. I am a new dad, a lover of the outdoors, and a Full Stack Web Developer.
-                    Since graduating college, I have been searching for a career that was right for me, and then I found Web Development.<br/><br/>
-                    I love problem solving because it just feels so good when you finally fix what needed fixing. I also love the idea of 
-                    constantly being able to learn and grow. In just 12 weeks, I went from having no coding knowledge, to learning Html, Css,
-                    Javascript, Sass, React, NodeJs, Npm, and Socket.io.<br/><br/> I know that this is just the surface for what there is to learn.
-                    I hope to grow into a skillful and knowledgable developer, and be able to solve the problems that need solving.
-                </p>
+                <span className="about-left__hover"><p className="about-left__text">Kaleb Armbrust</p></span>
             </div>
         </div>
     )
